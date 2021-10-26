238c8a4331feed66b83e1ec718e3206428509ed2
Vyavahare Nikhil <88494409+nikhilvyavahare18@users.noreply.github.com>
2021-10-26 12:26:27 +0530

    Update Palindrom_Number.java

--- String/src/Palindrom_Number.java
+++ String/src/Palindrom_Number.java
@@ -5,6 +5,9 @@ public class Palindrom_Number
 	public static void main(String[] args) 
 	{
 		String s="madam";
+		
+		System.out.println(s.length);
+		
 		String rev="";
 		
 		for(int i=s.length()-1;i>=0;i--)
